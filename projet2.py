639211a2e3087266463c583dcf52025caf8e21b7
3701214 <issamou.b@hotmail.fr>
2018-11-29 16:30:43 +0100

    carmichael fixed

--- projet2.py
+++ projet2.py
@@ -85,8 +85,11 @@ def isCarmichaelNumber( N ) :
     for all b ranging from 1 to n such that b and 
     n are relatively prime, i.e, gcd(b, n) = 1 
     
+    On vérifie en meme temps si c'est un nombre premier
+    
     https://www.geeksforgeeks.org/carmichael-numbers/
     """
+    prime = True
     b = 2
     while ( b < N ): 
           
@@ -97,8 +100,10 @@ def isCarmichaelNumber( N ) :
             # return false. 
             if ( projet.my_expo_mod(b, N - 1, N) != 1 ): 
                 return False
+        else:
+            prime = False
         b = b + 1
-    return True
+    return not prime
 
 """ test Carmichael """
 #print(isCarmichaelNumber(561))
@@ -130,5 +135,88 @@ def gen_carmichael( N ):
     
     sys.stdout.write('\n')
     return cars
+
+""" Test génération de Carmichael : ATTENTION prend du temps quand même ! """    
+#print(gen_carmichael(3000))
+
+
+def facteurpremier(n):
+    """int->liste[int]
+       renvoi la liste des facteurs premierd de n."""
+
+    s=2
+    L=[]
+    x=int(n**0.5)+1
+    if first_test(n)==True:   
+        return n
+    else:
+        for s in range (2,x):
+            if n%s==0:
+                if first_test(s)==True:
+                    L.append(s)
+    return L
+
+from random import randint
+
+def primal_under( N ):
+    """
+    input:
+    N = entier
+    -----------
+    output:
+    _sum = combien de nombres entiers sont inférieurs ou égale à N
+    """
     
-print(isCarmichaelNumber(563))
+    _sum = []
+    for i in range(N+1):
+        
+        """ progress bar """
+        sys.stdout.write('\r')
+        # the exact output you're looking for:
+        sys.stdout.write("[%-20s] %d%%" % ('=='*(int(10*(i+1)/(N+1))+1), int(100*(i/(N+1)))))
+        sys.stdout.flush()
+        sleep(0.25)
+        """ progress bar end """
+        
+        if( first_test(i) ):
+            _sum.append(i)
+    
+    sys.stdout.write('\n')
+    return _sum
+
+""" Generation de Carmichael à 3 facteurs premiers """
+import itertools
+from random import shuffle
+
+def gen_carmichael_3fp( N ):
+    liste = primal_under( int(math.sqrt(N)) )
+    if( liste == [] ):
+        return -1
+    perm = list(itertools.combinations(liste, 3))
+    shuffle(perm)
+    x = -1
+    for p in perm:
+        x = p[0]*p[1]*p[2]
+        if( isCarmichaelNumber(x) ):
+            return x
+    return -1
+
+import big_o
+def complexity_isCarmichaelNumber( NbTests, intRange ):
+    """
+    input:
+    
+    NbTests = nombre de tests à effectuer avec la fonction my_inverse_tuple
+    intRange = défini une borne maximum à ne pas dépasser pour la generation de données en entrée de la fonction ( generation d'un entier entre 0 et intRange )
+    --------
+    output:
+    
+    complexité expérimentale de la fonction my_inverse_tuple ( de préférence donner de grandes valeurs NbTests et intRange pour être précis )
+    """
+    data_generator = lambda n: randint(0,intRange)
+    
+    best, others = big_o.big_o(isCarmichaelNumber, data_generator, n_repeats=NbTests)
+    
+    print(best)
+    
+print(gen_carmichael_3fp(570))
