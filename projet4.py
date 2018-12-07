77bc2adaa384b4056616b3947256940f6dc9f663
chubby47 <issamou.b@hotmail.fr>
2018-12-07 05:26:47 +0100

    Update projet4.py

--- projet4.py
+++ projet4.py
@@ -9,60 +9,6 @@ import projet
 import random
 import projet2
 
-def is_prime( n, k = 7 ):
-   """use Rabin-Miller algorithm to return True (n is probably prime)
-      or False (n is definitely composite)"""
-   if n < 6:  # assuming n >= 0 in all cases... shortcut small cases here
-      return [False, False, True, True, False, True][n]
-   elif n & 1 == 0:  # should be faster than n % 2
-      return False
-   else:
-      s, d = 0, n - 1
-      while d & 1 == 0:
-         s, d = s + 1, d >> 1
-      # Use random.randint(2, n-2) for very large numbers
-      for a in random.sample(range(2, min(n - 2, 1000000000)), min(n - 4, k)):
-         x = pow(a, d, n)
-         if x != 1 and x + 1 != n:
-            for r in range(1, s):
-               x = pow(x, 2, n)
-               if x == 1:
-                  return False  # composite for sure
-               elif x == n - 1:
-                  a = 0  # so we know loop didn't continue to end
-                  break  # could be strong liar, try another a
-            if a:
-               return False  # composite if we reached end of this loop
-      return True  # probably prime if reached end of outer loop
-      
-def miller_rabin(n, k=10):
-	if n == 2 or n == 3:
-		return True
-	if not n & 1:
-		return False
-
-	def check(a, s, d, n):
-		x = pow(a, d, n)
-		if x == 1:
-			return True
-		for i in range(s - 1):
-			if x == n - 1:
-				return True
-			x = pow(x, 2, n)
-		return x == n - 1
-
-	s = 0
-	d = n - 1
-
-	while d % 2 == 0:
-		d >>= 1
-		s += 1
-
-	for i in range(k):
-		a = random.randrange(2, n - 1)
-		if not check(a, s, d, n):
-			return False
-	return True
 
 """ Test de primalité de Miller-Rabin """
 def test_miller_rabin( N, T = 7 ):
